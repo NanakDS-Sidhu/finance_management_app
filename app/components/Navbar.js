beceee479c8de8ae9b424629ae84a891f47f9340
nanak <sidhu.nds22@gmail.com>
2023-06-02 19:19:27 +0530

    minor changes

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,15 +4,15 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 const Navbar = () => {
     const { data, status } = useSession();
     return (
-        <div>
-            <h1>Navbar</h1>
-            {status === 'loading' && <h1> loading... please wait</h1>}
+        <div className='flex'>
+            <span>Navbar |</span>
+            {status === 'loading' && <span> loading... please wait</span>}
             {status === 'authenticated' ? (
-                <div>
-                    <h1> hi {data.user.name}</h1>
-                    <img src={data.user.image} alt={data.user.name + ' photo'} />
-                    <button onClick={signOut}>sign out</button>
-                </div>
+                <>
+                    <span className=' mr-2'> hi {data.user.name}</span>
+                    <img width={25} height={25} src={data.user.image} alt={data.user.name + ' photo'} />
+                    <button className='ml-2' onClick={signOut}>sign out</button>
+                </>
             ): <button onClick={() => signIn('google')}>sign in with gooogle</button>
             }
         </div>
---
+++ app/transactions/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TransactionList from '../components/transactions/TransactionList'
+
+const page = () => {
+  return (
+    <div>
+      <TransactionList></TransactionList>
+    </div>
+  )
+}
+
+export default page
