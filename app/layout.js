1b688c32cd63ace6a9e6b3bc1a4df8d1333f0edf
nanak <sidhu.nds22@gmail.com>
2023-06-05 11:49:55 +0530

    fixed background image error

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,11 @@
 "use client";
-
 import { SessionProvider } from "next-auth/react";
 import Navbar from "./components/Navbar";
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html className=" bg-no-repeat bg-cover bg-fixed text-white" style={{backgroundImage: 'url("blob-scene-haikei.svg")'}}>
+    <html className=" bg-no-repeat bg-cover bg-fixed text-white bg-my_bg_image" >
       <head />
       <body className="backdrop-blur-3xl min-h-screen">
         <SessionProvider>
