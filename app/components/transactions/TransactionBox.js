ec3e561d911f00a919cf17a701111c1325f8ecba
nanak <sidhu.nds22@gmail.com>
2023-06-05 23:00:05 +0530

    Fixed transaction type bug

--- app/components/transactions/TransactionBox.js
+++ app/components/transactions/TransactionBox.js
@@ -10,7 +10,7 @@ export default function TransactionBox(props){
         <TableData type="first" >{props.index+1}</TableData>
         <TableData >{props.transaction.account}</TableData>
         <TableData >{props.transaction.isIncome?<span className=" text-green-400">{props.transaction.amount}</span>:<span className=" text-red-400">{props.transaction.amount}</span>}</TableData>
-        <TableData >{props.transaction.transactionType?<span className=" text-yellow-400">Shopping</span>:<span className=" text-purple-300">Food</span>}</TableData>
+        <TableData ><span className=" text-purple-300">{props.transaction.transactionType}</span></TableData>
         <TableData >{props.transaction.createdAt.toDateString()}</TableData>
         <TableData type="last">{props.transaction.isIncome?<span className=" text-green-400">Income</span>:<span className=" text-red-400">Expense</span>}</TableData>
         </tr>
